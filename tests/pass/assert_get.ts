08665a63e28550080c7ed22b9ebbcfed5de577f4
delefrati <douglasdelefrati@gmail.com>
2021-03-04 22:51:12 -0300

    fix get

---
+++ tests/pass/assert_get.ts
@@ -0,0 +1,39 @@
+type Ty = {
+    x: "point",
+    y: number,
+    z: number,
+};
+
+type AnotherTy = {
+    x: "another point",
+    y: number,
+};
+
+
+type alias_ty=   AnotherTy | Ty;
+
+
+const x_type : alias_ty = {
+    x: "another point",
+    y: 2,
+};
+
+const y_type : Ty = {
+    x : "point",
+    y: 10,
+    z: 10,
+};
+
+function f_get(x: alias_ty) : number  {
+    return x.y;
+}
+function g_get(x: Ty) : "point" {
+    return x.x;
+}
+function h_get(x: AnotherTy | Ty) : number  {
+    return x.y;
+}
+
+//console.assert(f_get(x_type)=== 2);
+console.assert(g_get(y_type)=== "point");
+console.assert(h_get(x_type)=== 2);
